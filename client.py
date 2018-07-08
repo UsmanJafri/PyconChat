08dffe9dcbadcb63a1c4af521c52eb00e319139d
UsmanJafri <usmanjafri@hotmail.com>
2018-07-08 22:59:53 +0500

    Added server-side Group class, improved handshaking, added ability to view pending join requests

--- client.py
+++ client.py
@@ -1,24 +1,46 @@
 import socket
 import threading
+import pickle
+
+username = ""
+groupname = ""
 
 def serverListen(serverSocket):
-	pass
+	while True:
+		msg = serverSocket.recv(1024).decode("utf-8")
+		print(msg)
+		if msg == "/viewRequests":
+			serverSocket.send(bytes(username,"utf-8"))
+			serverSocket.recv(1024)
+			serverSocket.send(bytes(groupname,"utf-8"))
+			print(pickle.loads(serverSocket.recv(1024)))
 
 def userInput(serverSocket):
-	username = input("Welcome to PyconChat! Please enter your username: ")
-	groupname = input("Please enter the name of the group: ")
-	serverSocket.send(b"/register")
-	serverSocket.recv(1024)
-	serverSocket.send(bytes(username,"utf-8"))
-	serverSocket.recv(1024)
-	serverSocket.send(bytes(groupname,"utf-8"))
+	while True:
+		userInput = input()
+		if userInput == "1":
+			serverSocket.send(b"/viewRequests")
 
 if __name__ == "__main__":
 	ip = '127.0.0.1'
 	port = 8000
 	serverSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 	serverSocket.connect((ip,port))
-	serverListenThread = threading.Thread(target=serverListen,args=(serverSocket,))
+	username = input("Welcome to PyconChat! Please enter your username: ")
+	groupname = input("Please enter the name of the group: ")
 	userInputThread = threading.Thread(target=userInput,args=(serverSocket,))
-	serverListenThread.start()
 	userInputThread.start()
+	serverSocket.send(bytes(username,"utf-8"))
+	serverSocket.recv(1024)
+	serverSocket.send(bytes(groupname,"utf-8"))
+	response = serverSocket.recv(1024).decode("utf-8")
+	if response == "/adminReady":
+		print("You have created the group",groupname,"and are now an admin.")
+	elif response == "/ready":
+		print("You have joined the group",groupname)
+	elif response == "/wait":
+		print("Your request to join the group is pending admin approval.")
+		serverSocket.recv(1024)
+		print("You have joined the group",groupname)
+	serverListenThread = threading.Thread(target=serverListen,args=(serverSocket,))
+	serverListenThread.start()
--- server.py
+++ server.py
@@ -1,16 +1,31 @@
 import socket
 import threading
+import pickle
 
-def pyconChat(listenSocket):
+groups = {}
+
+class Group:
+	def __init__(self,admin,client):
+		self.admin = admin
+		self.clients = {}
+		self.offlineMessages = {}
+		self.allMembers = set()
+		self.onlineMembers = set()
+		self.joinRequests = set()
+
+		self.clients[admin] = client
+		self.allMembers.add(admin)
+		self.onlineMembers.add(admin)
+
+def pyconChat(client):
 	while True:
-		client,_ = listenSocket.accept()
 		msg = client.recv(1024).decode("utf-8")
-		if msg == "/register":
-			client.send(b"/sendUsername")
+		if msg == "/viewRequests":
+			client.send(b"/viewRequests")
 			username = client.recv(1024).decode("utf-8")
 			client.send(b"/sendGroupname")
 			groupname = client.recv(1024).decode("utf-8")
-
+			client.send(pickle.dumps(groups[groupname].joinRequests))
 
 if __name__ == "__main__":
 	ip = '127.0.0.1'
@@ -19,5 +34,21 @@ if __name__ == "__main__":
 	listenSocket.bind((ip,port))
 	listenSocket.listen(10)
 	print("PyconChat Server running")
-	pyconChatThread = threading.Thread(target=pyconChat, args=(listenSocket,))
-	pyconChatThread.start()
+	while True:
+		client,_ = listenSocket.accept()
+		username = client.recv(1024).decode("utf-8")
+		client.send(b"/sendGroupname")
+		groupname = client.recv(1024).decode("utf-8")
+		if groupname in groups:
+			if username in groups[groupname].allMembers:
+				groups[groupname].onlineMembers.add(username)
+				threading.Thread(target=pyconChat, args=(client,)).start()
+				client.send(b"/ready")
+			else:
+				groups[groupname].joinRequests.add(username)
+				client.send(b"/wait")
+		else:
+			groups[groupname] = Group(username,client)
+			threading.Thread(target=pyconChat, args=(client,)).start()
+			client.send(b"/adminReady")
+			print("New group:",groupname,"| Admin:",username)
