391261f6615c12aa1699b3684b1fc925bb55662b
UsmanJafri <usmanjafri@hotmail.com>
2018-07-09 22:45:10 +0500

    Added group chat functionality (Disconnection bugged)

--- client.py
+++ client.py
@@ -3,6 +3,7 @@ import threading
 import pickle
 
 state = {}
+test = threading.Lock()
 
 def serverListen(serverSocket):
 	while True:
@@ -29,9 +30,11 @@ def serverListen(serverSocket):
 			serverSocket.send(bytes(state["groupname"],"utf-8"))
 			response = serverSocket.recv(1024).decode("utf-8")
 			if response == "/proceed":
+				state["inputMessage"] = False
 				print("Please enter the username to approve: ")
 				with state["inputCondition"]:
 					state["inputCondition"].wait()
+				state["inputMessage"] = True
 				serverSocket.send(bytes(state["userInput"],"utf-8"))
 				print(serverSocket.recv(1024).decode("utf-8"))
 			else:
@@ -44,19 +47,33 @@ def serverListen(serverSocket):
 			serverSocket.shutdown(socket.SHUT_RDWR)
 			serverSocket.close()
 			break
+		elif msg == "/messageSend":
+			serverSocket.send(bytes(state["username"],"utf-8"))
+			serverSocket.recv(1024)
+			serverSocket.send(bytes(state["groupname"],"utf-8"))
+			serverSocket.recv(1024)
+			serverSocket.send(bytes(state["userInput"],"utf-8"))
+			test.release()
+		else:
+			print(msg)
 
 def userInput(serverSocket):
 	while True:
+		test.acquire()
 		state["userInput"] = input()
+		test.release()
 		with state["inputCondition"]:
 			state["inputCondition"].notify()
-		if state["userInput"] == "1":
+		if state["userInput"] == "/1":
 			serverSocket.send(b"/viewRequests")
-		elif state["userInput"] == "2":
+		elif state["userInput"] == "/2":
 			serverSocket.send(b"/approveRequest")
-		elif state["userInput"] == "3":
+		elif state["userInput"] == "/3":
 			serverSocket.send(b"/disconnect")
 			break
+		elif state["inputMessage"]:
+			test.acquire()
+			serverSocket.send(b"/messageSend")
 
 if __name__ == "__main__":
 	ip = '127.0.0.1'
@@ -67,6 +84,7 @@ if __name__ == "__main__":
 	state["username"] = input("Welcome to PyconChat! Please enter your username: ")
 	state["groupname"] = input("Please enter the name of the group: ")
 	state["alive"] = True
+	state["inputMessage"] = True
 	userInputThread = threading.Thread(target=userInput,args=(serverSocket,))
 	userInputThread.start()
 	serverSocket.send(bytes(state["username"],"utf-8"))
@@ -79,8 +97,6 @@ if __name__ == "__main__":
 		print("You have joined the group",state["groupname"])
 	elif response == "/wait":
 		print("Your request to join the group is pending admin approval.")
-		serverSocket.recv(1024)
-		print("You have joined the group",state["groupname"])
 	serverListenThread = threading.Thread(target=serverListen,args=(serverSocket,))
 	serverListenThread.start()
 	while True:
--- server.py
+++ server.py
@@ -25,9 +25,15 @@ class Group:
 		self.onlineMembers.add(username)
 		self.clients[username] = client
 
+	def sendMessage(self,message,username):
+		for member in self.onlineMembers:
+			if member != username:
+				self.clients[member].send(bytes(username + ": " + message,"utf-8"))
+
 def pyconChat(client):
 	while True:
 		msg = client.recv(1024).decode("utf-8")
+		print(msg)
 		if msg == "/viewRequests":
 			client.send(b"/viewRequests")
 			username = client.recv(1024).decode("utf-8")
@@ -62,6 +68,14 @@ def pyconChat(client):
 			client.send(b"/sendGroupname")
 			groupname = client.recv(1024).decode("utf-8")
 			groups[groupname].disconnect(username)
+		elif msg == "/messageSend":
+			client.send(b"/messageSend")
+			username = client.recv(1024).decode("utf-8")
+			client.send(b"/sendGroupname")
+			groupname = client.recv(1024).decode("utf-8")
+			client.send(b"/sendMessage")
+			message = client.recv(1024).decode("utf-8")
+			groups[groupname].sendMessage(message,username)
 
 if __name__ == "__main__":
 	ip = '127.0.0.1'
